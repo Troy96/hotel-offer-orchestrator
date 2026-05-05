8deb3f2593c6384ea5fb63a2b75462d54069b129
Tuhin Roy <troy0870@gmail.com>
2026-05-05 10:49:35 +0530

    setup for dockerize the app and worker

--- apps/api/src/temporalClient.ts
+++ apps/api/src/temporalClient.ts
@@ -1,6 +1,9 @@
 import { Connection, Client } from "@temporalio/client";
 
 export async function getTemporalClient() {
-  const connection = await Connection.connect();
+  const connection = await Connection.connect({
+    address: process.env.TEMPORAL_ADDRESS || "127.0.0.1:7233",
+  });
+
   return new Client({ connection });
 }
--- apps/worker/src/worker.ts
+++ apps/worker/src/worker.ts
@@ -2,9 +2,7 @@ import { Worker, NativeConnection } from "@temporalio/worker";
 import * as activities from "../../../packages/workflows/src/activities";
 
 async function run() {
-    const connection = await NativeConnection.connect({
-        address: "127.0.0.1:7233", // force IPv4
-    });
+    const connection = await connectTemporalWithRetry();
 
   const worker = await Worker.create({
     connection,
@@ -21,3 +19,17 @@ run().catch(err => {
   console.error(err);
   process.exit(1);
 });
+
+async function connectTemporalWithRetry(retries = 5) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await NativeConnection.connect({
+        address: process.env.TEMPORAL_ADDRESS || "127.0.0.1:7233",
+      });
+    } catch (err) {
+      console.log("Retrying Temporal connection...");
+      await new Promise(res => setTimeout(res, 2000));
+    }
+  }
+  throw new Error("Failed to connect to Temporal");
+}
